daf5333af2cbd46fbab93dd6930970343b71c495
maoshuochen <maoshuochen@gmail.com>
2022-04-05 19:59:38 +0800

    Done!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # [URL Shortener Microservice](https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice)
+
+Course Link: https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice
--- server.js
+++ server.js
@@ -1,24 +1,50 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
 const app = express();
+let urls = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+//phrase post body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/shorturl", function (req, res) {
+    let oringinalUrl = req.body.url;
+    let shortUrl = Math.floor(Math.random() * 10000);
+    if (
+        oringinalUrl.split("//")[0] === "https:" ||
+        oringinalUrl.split("//")[0] === "http:"
+    ) {
+        urls.push({ oringinalUrl, shortUrl });
+    } else {
+        res.json({ error: "invalid url" });
+    }
+    res.json({ original_url: oringinalUrl, short_url: shortUrl });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get("/api/shorturl/:shortUrl", function (req, res) {
+    let shortUrl = Number(req.params.shortUrl);
+    let url = urls.find((url) => url.shortUrl === shortUrl);
+    if (url) {
+        res.redirect(url.oringinalUrl);
+        console.log({ shortUrl, url });
+    } else {
+        res.json({ error: "not in list" });
+    }
+});
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
